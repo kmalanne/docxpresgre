e36289d6e73d6779058b150e6bb94bbaade644eb
Kai-Mikael Alanne <Kaitsu@MacBook-Pro.home>
2017-02-21 20:14:14 +0200

    Fix unit tests

--- app/test/test.js
+++ app/test/test.js
@@ -6,31 +6,31 @@ const api = supertest(app);
 
 test('GET /', (t) => {
   api
-  .get('/')
-  .expect('Content-type', 'text/html; charset=utf-8')
-  .expect(200)
-  .end((err, res) => {
-    if (err) {
-      t.fail(err);
-    } else {
-      t.ok(res.body, 'It should have a response body');
-    }
-    t.end();
-  });
+    .get('/api/v1/')
+    .expect('Content-type', 'text/html; charset=utf-8')
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        t.fail(err);
+      } else {
+        t.ok(res.body, 'It should have a response body');
+      }
+      t.end();
+    });
 });
 
 test('GET unknown route', (t) => {
   api
-  .get('/scam')
-  .expect(404)
-  .end((err, res) => {
-    if (err) {
-      t.fail(err);
-    } else {
-      t.ok(res.body, 'It should respond 404');
-    }
-    t.end();
-  });
+    .get('/api/v1/scam')
+    .expect(404)
+    .end((err, res) => {
+      if (err) {
+        t.fail(err);
+      } else {
+        t.ok(res.body, 'It should respond 404');
+      }
+      t.end();
+    });
 });
 
 test.onFinish(() => process.exit(0));
