43e7dfd0642d1bec02c63c3b98a3dad2e92a7a29
Kai-Mikael Alanne <Kaitsu@MacBook-Pro.home>
2017-02-22 21:10:04 +0200

    Fix seeds

--- src/db/seeds/init.js
+++ src/db/seeds/init.js
@@ -1,20 +1,20 @@
 exports.seed = (knex, Promise) =>
-  knex('table').del().then(() =>
-    Promise.all([
-      knex('table').insert({
-        id: 1,
-        column_1: 'value1',
-        column_2: 1,
-      }),
-      knex('table').insert({
-        id: 2,
-        column_1: 'value2',
-        column_2: 2,
-      }),
-      knex('table').insert({
-        id: 3,
-        column_1: 'value3',
-        column_2: 3,
-      }),
-    ]),
-  );
+  knex('table').del()
+    .then(() =>
+      Promise.all([
+        knex('table').insert({
+          id: 1,
+          column_1: 'value1',
+          column_2: 1,
+        }),
+        knex('table').insert({
+          id: 2,
+          column_1: 'value2',
+          column_2: 2,
+        }),
+        knex('table').insert({
+          id: 3,
+          column_1: 'value3',
+          column_2: 3,
+        }),
+      ]));
