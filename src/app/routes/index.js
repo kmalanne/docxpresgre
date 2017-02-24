65cb934ed1880e5529694a50977c44c29ea209c7
Kai-Mikael Alanne <kaitsu@futurice.com>
2017-02-24 19:22:44 +0200

    Fix index

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Indexx'));
+router.get('/', (req, res) => res.send('Index'));
 
 module.exports = router;
